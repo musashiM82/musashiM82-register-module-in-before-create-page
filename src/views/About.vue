87aefec97fffc5155e68c634e2044e60db0020e0
peppe82 <>
2020-11-20 15:48:05 +0100

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">about page</div>
+  <h2>state.common.count {{ commonCountGetter }}</h2>
+  <h2>state.about.count {{ aboutCountGetter }}</h2>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { mapGetters, mapMutations, mapActions } from "vuex";
+import aboutStore from "../store/aboutStore";
+
+@Options({
+  computed: {
+    ...mapGetters("common", ["commonCountGetter"]),
+    ...mapGetters("about", ["aboutCountGetter"]),
+  },
+})
+export default class About extends Vue {
+  beforeCreate() {
+    if (!this.$store.state.about) {
+      this.$store.registerModule("about", aboutStore);
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">home page</div>
+  <h2>state.common.count {{ commonCountGetter }}</h2>
+  <h2>state.home.count {{ countHomeGetter }}</h2>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { mapGetters, mapMutations, mapActions } from "vuex";
+import homeStore from "../store/homeStore";
+
+@Options({
+  computed: {
+    ...mapGetters("common", ["commonCountGetter"]),
+    ...mapGetters("home", ["countHomeGetter"]),
+  },
+})
+export default class Home extends Vue {
+  beforeCreate() {
+    if (!this.$store.state.home) {
+      this.$store.registerModule("home", homeStore);
+    }
+  }
+}
+</script>
